2286d09b9e704fb393d65da9f6917f01aeffc288
lukaszsl <lukasz.sliczner@interia.pl>
2019-03-17 21:46:59 +0000

    Change language to English

--- servo.c
+++ servo.c
@@ -1,22 +1,22 @@
-#include <Servo.h> //Biblioteka odpowiedzialna za serwa
+#include <Servo.h> // Library responsible for servos
 
-Servo serwomechanizm;  //Tworzymy obiekt, dzięki któremu możemy odwołać się do serwa
-int pozycja = 0; //Aktualna pozycja serwa 0-180
-int zmiana = 6; //Co ile ma się zmieniać pozycja serwa?
+Servo serwomechanism;  // Create an object that allows us to refer to the servo
+int position = 0; // Current servo position
+int shift = 6; // How much servo position has to change at one step
 
 void setup()
 {
-  serwomechanizm.attach(9);  //Serwomechanizm podłączony do pinu 9
+	serwomechanism.attach(9);  // Connect the servo to pin 9
 }
 
 void loop()
 {
-  if (pozycja < 180) { //Jeśli pozycja mieści się w zakresie
-    serwomechanizm.write(pozycja); //Wykonaj ruch
-  } else { //Jeśli nie, to powrót na początek
-    pozycja = 0;
-  }
+	if (position < 180) { // If position is in the range
+		serwomechanism.write(position); // Make a move
+	} else { // If not, return to start position
+		position = 0;
+	}
 
-  pozycja = pozycja + zmiana; //Zwiększenie aktualnej pozycji serwa
-  delay(200); //Opóźnienie dla lepszego efektu
+	position = position + shift; // Increase the current servo position
+	delay(200);
 }
