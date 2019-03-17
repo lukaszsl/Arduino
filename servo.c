2037acc400c659288b34e778f7a82ac1cc54e766
lukaszsl <lukasz.sliczner@interia.pl>
2019-03-17 22:03:54 +0000

    Change servo cycle delay every time

--- servo.c
+++ servo.c
@@ -3,6 +3,7 @@
 Servo serwomechanism;  // Create an object that allows us to refer to the servo
 int position = 0; // Current servo position
 int shift = 6; // How much servo position has to change at one step
+int cycleDelay = 200;
 
 void setup()
 {
@@ -15,8 +16,9 @@ void loop()
 		serwomechanism.write(position); // Make a move
 	} else { // If not, return to start position
 		position = 0;
+		cycleDelay = 200;
 	}
 
 	position = position + shift; // Increase the current servo position
-	delay(200);
+	delay(cycleDelay += 50);
 }
